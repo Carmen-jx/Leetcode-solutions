58cd60a5ecce27ef86261838bfca89e65c2d3ffa
Carmen Lam <148829232+Carmen-jx@users.noreply.github.com>
2024-06-18 13:57:12 +0000

    added string compression

---
+++ Array and strings/443. String Compression/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+
+     
+        result =0
+        i = 0
+
+        while i < len(chars):
+            letter = chars[i]
+            count = 0
+            while i < len(chars) and chars[i] == letter:
+                count += 1
+                i += 1
+            chars[result] = letter
+            result += 1
+            if count > 1:
+                for c in str(count):
+                    chars[result] = c
+                    result +=1
+            
+        return result
